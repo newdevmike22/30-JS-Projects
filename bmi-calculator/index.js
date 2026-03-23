ef72c64d72d8d718c40daf3cf13582764ad1f975
newdevmike22 <paola@dominicansoul.com>
2026-03-23 08:36:19 -0400

    BMI Calculator, Clock, and Temperature Converter Projects
    BMI Calculator, Clock, and Temperature Converter Projects created with HTML, CSS, and JS.

---
+++ bmi-calculator/index.js
@@ -0,0 +1,24 @@
+const btnEl = document.getElementById("btn");
+const bmiInput = document.getElementById("bmi-result");
+const weightConditionEl = document.getElementById("weight-condition");
+
+const calculateBMI = () => {
+  const heightValue = document.getElementById("height").value / 100;
+  const weightValue = document.getElementById("weight").value;
+
+  const bmiValue = weightValue / (heightValue * heightValue);
+
+  bmiInput.value = bmiValue.toFixed(2);
+
+  if (bmiValue < 18.5) {
+    weightConditionEl.innerText = "Under weight";
+  } else if (bmiValue >= 18.5 && bmiValue <= 24.9) {
+    weightConditionEl.innerText = "Normal weight";
+  } else if (bmiValue >= 25 && bmiValue <= 29.9) {
+    weightConditionEl.innerText = "Over weight";
+  } else if (bmiValue >= 30) {
+    weightConditionEl.innerText = "Obesity";
+  }
+};
+
+btnEl.addEventListener("click", calculateBMI);
